9abc49ab4112fd49dfb9f8a417c70425ecdca506
GPA Tracker <gpa-tracker@example.com>
2026-06-19 13:49:45 +0800

    添加缓存控制头，禁用页面缓存

--- server.js
+++ server.js
@@ -267,6 +267,9 @@ app.get('/health', (req, res) => {
 
 // 页面路由
 app.get('*', (req, res) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
